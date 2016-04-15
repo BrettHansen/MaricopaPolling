05d2d4d3e69cfdcb07fe083c86f21b43e284ed59
Brett Hansen <hansen.brett7@gmail.com>
2016-04-15 15:15:31 -0700

    Polling location data parsed.

--- pollingLocations.js
+++ pollingLocations.js
@@ -1,5 +1,17 @@
+locations = [];
 $.get("locations.txt", function(data) {
-	console.log(data);
+	var lines = data.split('\n');
+	console.log(lines);
+	for(i in lines) {
+		elements = lines[i].split('\t');
+		location = new Object();
+		location.city = elements[0];
+		location.name = elements[1];
+		location.address = elements[2] + ", " + elements[3];
+		location.lat = elements[4];
+		location.lng = elements[5];
+		console.log(location);
+	}
 }, "text");
 
 console.log("here");
